362e2e290e1e373020a779f81e82091e498eefd9
Kacper Chmielewski <kchmielewski@tlen.pl>
2022-11-30 02:34:17 +0100

    Adding search bar to mapbox

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 // Connects to data-controller="map"
 export default class extends Controller {
@@ -18,6 +19,9 @@ export default class extends Controller {
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
     this.#geolocalisation()
+
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                         mapboxgl: mapboxgl }))
   }
 
   #addMarkersToMap() {
@@ -59,5 +63,5 @@ export default class extends Controller {
       geolocate.trigger();
       });
   }
-  
+
 }
