24d172331ce86ea257d2cc39eb80f971458586b2
Boris Mwnaga <borismwanga@gmail.com>
2022-12-02 12:08:48 +0100

    routes fixed, profil photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
 
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
