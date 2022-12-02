0c8d5c0bcc8d6615d469650d4d9ba43b345cf9dd
Boris Mwnaga <borismwanga@gmail.com>
2022-12-02 14:37:17 +0100

    fixed some bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
-
+  before_action :configure_permitted_parameters, if: :devise_controller?  
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :photo])
   end
 end
