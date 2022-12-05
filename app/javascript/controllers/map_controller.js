0d9c9024e37763c11dab351dc6332b05e1a39d02
Xeniouska <115425494+Xeniouska@users.noreply.github.com>
2022-12-05 10:01:19 +0100

    change color and size of dots

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -54,8 +54,8 @@ export default class extends Controller {
       customMarker.className = "marker"
       customMarker.style.backgroundImage = `url('${marker.image_url}')`
       customMarker.style.backgroundSize = "contain"
-      customMarker.style.width = "16px"
-      customMarker.style.height = "16px"
+      customMarker.style.width = "20px"
+      customMarker.style.height = "20px"
       customMarker.style.opacity = "50%"
 
       new mapboxgl.Marker(customMarker)
