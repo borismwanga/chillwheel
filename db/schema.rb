60b79551b51a6b1d0b300208552d1517268cd711
Kacper Chmielewski <kchmielewski@tlen.pl>
2022-12-06 12:20:15 +0100

    Welcome mail in testing mode works

--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_091209) do
     t.text "comment"
     t.string "category"
     t.string "location"
-    t.boolean "acitve", default: true, null: false
+    t.boolean "active", default: true, null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
