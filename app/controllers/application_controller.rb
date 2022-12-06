b4a66fd7534f5fa2698cedd924ec0c76350395a5
Kacper Chmielewski <kchmielewski@tlen.pl>
2022-12-06 12:59:35 +0100

    Letter Opener - gem added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?  
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
