be418ee16ecc63e1b23f852aaa73748750d7f942
thomas merle <thomasmerle.pro@gmail.com>
2022-12-08 16:09:54 +0100

    stelon bike page done

--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_103038) do
     t.text "comment"
     t.string "category"
     t.string "location"
-    t.boolean "active", default: true, null: false
+    t.boolean "acitve", default: true, null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
